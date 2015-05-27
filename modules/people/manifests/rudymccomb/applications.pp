34650dd85b1880918f0f705a29080d6d7657b2b1
acidburn <aciburn@acidburns-mbp.attlocal.net>
2015-05-26 22:34:29 -0700

    added iterm, skype, firefox

--- modules/people/manifests/rudymccomb/applications.pp
+++ modules/people/manifests/rudymccomb/applications.pp
@@ -3,11 +3,19 @@ class people::rudymccomb::applications {
   include chrome
   include chrome::canary
   include virtualbox
-  include docker
-  include spotify
-  include atom
-  
-  package { 'evernote': provider => 'brewcask' }
+  include dash
+  include evernote
+  include googledrive
+  include cyberduck
+  include firefox
+  include skype
+  #include docker
 
+  #package 'docker'{
+  #  ensure => absent
+  #}
+
+  include iterm2::stable
+  include iterm2::colors::solarized_dark
 
 }
